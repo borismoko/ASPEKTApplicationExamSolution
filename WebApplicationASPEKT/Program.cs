036214c19db5241e6a0d196afb86830a115db3d8
borismoko <borismokovski@gmail.com>
2025-02-21 16:43:27 +0100

    Added databse migration on every startup

--- WebApplicationASPEKT/Program.cs
+++ WebApplicationASPEKT/Program.cs
@@ -27,6 +27,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate(); 
+}
 
 
 // Configure the HTTP request pipeline.
